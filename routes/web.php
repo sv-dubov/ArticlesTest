cae4d8a895d30a1b3a02f895fddb0e0cf351e8b0
Sviatoslav2 <sviatoslav.dubov@itgroup.kz>
2024-02-14 01:42:02 +0200

    Redirect main page

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('front.articles.index');
 });
 
 Route::get('/change-language/{lang_code}', [LanguageController::class, 'changeLanguage'])->name('change_lang');
